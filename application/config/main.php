55f295f25faca615005a0d6c625474899b100998
dajjwwx <dajjwwx@163.com>
2015-12-09 15:28:22 +0800

    修改Sinaapp数据库信息

--- application/config/main.php
+++ application/config/main.php
@@ -8,7 +8,8 @@ return [
 		'db'=>[
 			'modelDirectory'=>__APP__.'/models',
 			'connections'=>[
-			    	'development' => 'mysql://root:blueidea@127.0.0.1/ykg',
+			    	// 'development' => 'mysql://root:blueidea@127.0.0.1/ykg',
+			    	'sinaapp'=>'mysql://'.SAE_MYSQL_USER.':'.SAE_MYSQL_PASS.'@'.SAE_MYSQL_HOST_M.'/app_ykgframework',
 			    	// 'production' => 'mysql://root:blueidea@127.0.0.1/ykg'
 			],
 			'class'=>'\YKG\base\Model',
