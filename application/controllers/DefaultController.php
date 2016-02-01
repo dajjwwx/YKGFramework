869806cd790dbb4a3da33291bc17703d41b70b6b
dajjwwx <dajjwwx@163.com>
2016-02-01 23:50:56 +0800

    转到Windows平台第一次上传

---
+++ application/controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+
+use \YKG\base\Controller;
+
+class DefaultController extends Controller
+{
+	public function  actionIndex()
+	{
+		echo "actionIndex";
+	}
+}
+?>
